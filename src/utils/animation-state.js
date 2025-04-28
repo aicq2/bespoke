27cd9a7c0ca86c7a8e218b84cdae9e5a0e12900a
Marina Al <alyohin@gmail.com>
2025-04-28 16:37:18 +0500

    remove utils

--- src/utils/animation-state.js
+++
@@ -1,9 +0,0 @@
-const animatedElements = new WeakSet();
-
-export const markAsAnimated = (element) => {
-  animatedElements.add(element);
-};
-
-export const isAnimated = (element) => {
-  return animatedElements.has(element);
-};
